bc5c9709a1b59e027c78b19f896b5de9b47bac14
おこめ <110918400+okomet@users.noreply.github.com>
2023-05-27 09:11:01 +0000

    戻る処理作成

--- src/index.js
+++ src/index.js
@@ -4,35 +4,47 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  const li = document.createElement("li");
+  createIncompleteList(inputText);
+};
 
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
+  const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
-
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
+  // 完了ボタン押下処理
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     const addTarget = completeButton.parentNode.parentNode;
-
     deleteFromIncompleteList(addTarget);
 
-    const text = addTarget.firstElementChild.innerText;
+    const text = addTarget.firstElementChild.firstElementChild.innerText;
 
     addTarget.textContent = null;
 
     const li = document.createElement("li");
-
     const div = document.createElement("div");
     div.className = "list-row";
-
     const p = document.createElement("p");
     p.innerText = text;
 
+    // 戻るボタン押下処理
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     div.appendChild(p);
     div.appendChild(backButton);
@@ -41,6 +53,7 @@ const onClickAdd = () => {
     document.getElementById("complete-list").appendChild(li);
   });
 
+  // 削除ボタン押下処理
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
@@ -53,10 +66,6 @@ const onClickAdd = () => {
   li.appendChild(div);
 
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
